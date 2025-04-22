33b89d7a90264504404fdd88605666572d0e4891
BlackMask <crotolandiaofc@gmail.com>
2025-04-22 09:44:04 -0500

    Create tokenizer.py

---
+++ tokenizer.py
@@ -0,0 +1,8 @@
+# tokenizer.py  
+class Terra7Tokenizer:  
+    def encode(self, text):  
+        # Tokenización al estilo BERT (subword)  
+        return tokens  
+
+    def decode(self, tokens):  
+        # Reconstruir texto desde tokens  
